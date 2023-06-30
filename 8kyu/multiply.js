47d70ab948a52631b2833b78e07246532638d849
Daniel Lee <d.lee084@gmail.com>
2023-06-30 12:12:11 -0700

    added new codewars challenge

---
+++ 8kyu/multiply.js
@@ -0,0 +1,12 @@
+//Multiply
+//Description:
+//This code does not execute properly. Try to figure out why.
+
+//My solution
+function multiply(a, b){
+    //add missing return statement 
+    return a * b;
+}
+
+//arrow syntax 
+const multiply = (a, b) => a * b;
