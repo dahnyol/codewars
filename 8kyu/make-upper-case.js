19f1f199d8971784d1c8c91891c7a51ae71c4ce5
Daniel Lee <d.lee084@gmail.com>
2023-07-02 09:23:26 -0700

    added new codewars challenge

---
+++ 8kyu/make-upper-case.js
@@ -0,0 +1,10 @@
+// MakeUpperCase 
+// Write a function which converts the input string to uppercase.
+
+//My solution
+function makeUpperCase(str){
+    return str.toUpperCase();
+}
+
+//arrow syntax
+const makeUpperCase = str => str.toUpperCase();
