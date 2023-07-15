626a6e2b9f0fa2974b2fcf4d82f847b78b99e4f0
Daniel Lee <d.lee084@gmail.com>
2023-07-15 16:39:43 -0700

    added a new codewars challenge

---
+++ 7kyu/two-to-one.js
@@ -0,0 +1,29 @@
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+function longest(s1, s2) {
+    let result = [...new Set((s1 + s2).split(''))] // spread operator to create Set object
+    // add both inputs together to merge into new object. Set constructor will only add unique elements. After split to turn it into an array
+                    .sort() //sort letters
+                    .join(''); //join array to return a string
+  return result;
+}
+
+function longest(s1, s2) {
+    let output = [];
+    let combi = s1.concat(s2);
+    let array = combi.split('').sort();
+ 
+    for (let i = 0; i <= array.length; i++) {
+        //console.log(array[i]);//test
+        if (!output.includes(array[i]) ) {
+              output.push(array[i]);
+        }
+    }
+    return output.join('');
+}
