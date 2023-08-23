bf3a7b0973cf9f8034d49d2e30e4c3fb49d77c0e
Daniel Lee <d.lee084@gmail.com>
2023-08-22 23:41:12 -0700

    added a new codewars

---
+++ 8kyu/reversed-words.js
@@ -0,0 +1,25 @@
+// Complete the solution so that it reverses all of the words within the string passed in.
+// Words are separated by exactly one space and there are no leading or trailing spaces.
+// Example(Input --> Output):
+// "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+//note refuse all words within string
+//P words seperated by one space, no leading or trailing spaces
+//R return reverse words of each word in string
+//E
+function reverseWords(sentence) {
+//P //use method split and reverse
+    //old way is init new sentence variable empty array
+    //iterate over sentence and add/push word to the empty array
+    //join the empty array together as a string and return it
+}
+
+
+const reverseWords = sentence => sentence.split(' ').reverse().join(' ');
+
+
+function reverseWords(str){
+    return str.split(' ')
+          .reverse()
+          .join(' ')
+}
