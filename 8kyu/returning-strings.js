e2aed3fe630434c0b34422c7ba491043018b6bb0
Daniel Lee <d.lee084@gmail.com>
2023-09-03 22:34:36 -0700

    added a new codewars

---
+++ 8kyu/returning-strings.js
@@ -0,0 +1,18 @@
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+//note: get name input and output string with input string 
+//P will get a string
+//R return a string with the input string
+//E
+function greet(name){
+//P  // return 'sentence' + name
+}
+
+
+function greet(name){
+    return 'Hello,' + ' ' + name + ' ' + 'how are you doing today?';
+}
+
+
+const greet = name => `Hello, ${name} how are you doing today?`;
