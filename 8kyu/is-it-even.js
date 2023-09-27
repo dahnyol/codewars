ad8f03b58a176b03d67a3636317469c7aaeb1dff
Daniel Lee <d.lee084@gmail.com>
2023-09-26 23:10:42 -0700

    added a new codewars

---
+++ 8kyu/is-it-even.js
@@ -0,0 +1,24 @@
+// In this Kata we are passing a number (n) into a function.
+// Your code will determine if the number passed is even (or not).
+// The function needs to return either a true or false.
+// Numbers may be positive or negative, integers or floats.
+// Floats with decimal part non equal to zero are considered UNeven for this kata.
+
+
+// NOTE: pass n into function and check if it is even, return t/f
+//P input may be pos or neg, int or floats
+//R return t or f
+//E
+function checkEven(n){
+//P // check if n % 2
+}
+
+
+function testEven(n) {
+    return n % 2 === 0;
+}
+
+
+const testEven = function(n){
+    return !(n % 2);
+}
