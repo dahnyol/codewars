091847cc26d4a3ccc206eb32495b505bf48de745
Daniel Lee <d.lee084@gmail.com>
2023-11-10 14:48:30 -0800

    added a some solutions to 8kyu/vowel-remover

---
+++ 8kyu/drink-about.js
@@ -0,0 +1,40 @@
+
+// Kids drink toddy.
+// Teens drink coke.
+// Young adults drink beer.
+// Adults drink whisky.
+
+// Make a function that receive age, and return what they drink.
+
+// Rules:
+
+// Children under 14 old.
+// Teens under 18 old.
+// Young under 21 old.
+// Adults have 21 or more.
+
+// Examples: (Input --> Output)
+
+// 13 --> "drink toddy"
+// 17 --> "drink coke"
+// 18 --> "drink beer"
+// 20 --> "drink beer"
+// 30 --> "drink whisky"
+
+function peopleWithAgeDrink(old) {
+    if (old > 20){
+        return 'drink whisky'
+    } if (old > 17) {
+        return 'drink beer'
+    } if (old > 13) {
+        return 'drink coke'
+    } else {
+        return 'drink toddy'
+    }
+};
+
+
+const peopleWithAgeDrink = (age) =>
+  age < 14 ? "drink toddy" :
+  age < 18 ? "drink coke" :
+  age < 21 ? "drink beer" : "drink whisky"
