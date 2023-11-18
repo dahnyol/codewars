28737d9b27cca1d771e307fde651c454b1de4375
Daniel Lee <d.lee084@gmail.com>
2023-11-18 14:26:24 -0800

    added a some solutions to 8kyu/vowel-remover

--- 8kyu/vowel-remover.js
+++ 8kyu/vowel-remover.js
@@ -19,3 +19,30 @@ function shortcut (string) {
    }
    return result;
 }
+
+
+function shortcut (string) {
+    return string
+      .split('')
+      .filter(str => !'aeiou'.includes(str))
+      .join('')
+}
+
+
+function shortcut(string){
+    var vowels = "aeiou";
+    var output = "";
+    for (var i = 0; i < string.length; i++) {
+      if (!(vowels.indexOf(string[i]) > -1)) {
+        output += string[i];
+      }
+    }
+    return output;
+}
+
+ 
+function shortcut(str) {
+    return str.split('').filter(function(e) {
+      return ['a', 'e', 'i', 'o', 'u'].indexOf(e) == -1 
+    }).join('')
+}
