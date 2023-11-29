3ffaa1f39a16220e7bac9f3701e12f01514d0976
Daniel Lee <d.lee084@gmail.com>
2023-11-29 02:19:24 -0800

    added afew solutions to five-without-numbers

--- 8kyu/five-without-numbers.js
+++ 8kyu/five-without-numbers.js
@@ -5,3 +5,21 @@
 // Good luck :)
 
 
+// create an array of items that has 5 items. 
+// return the length fo the array
+
+function unusualFive() {
+    let array = ['itemOne', 'itemTwo', 'itemThree', 'itemFour', 'itemFive'];
+      return array.length;
+}
+
+function ususualFive(){function unusualFive(){
+    let array = ['item1', 'item2', 'item3', 'item4', 'item5'];
+    return array.length;
+}
+    return [,,,,,].length;
+}
+
+function unusualFive(){
+    return "words".length;
+}
