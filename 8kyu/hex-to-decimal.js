ebc22c8fee49601de9b2f539b9e32bc4c108cbcd
Daniel Lee <d.lee084@gmail.com>
2024-01-24 00:36:29 -0800

    created hex-to-decimal.js

---
+++ 8kyu/hex-to-decimal.js
@@ -0,0 +1,10 @@
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+
+function hexToDec(hexString){
+    //hex number is given as a string
+    //parseInt(string, radix), hexString in string 
+    //radix is 16 because there are 16 hexadecimal digits (base-16)
+}
+
+const hexToDec = (hexString) => parseInt(hexString, 16)
